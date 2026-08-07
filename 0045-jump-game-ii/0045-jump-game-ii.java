15b6099014da02056930172cf4d823f299f73af9
Supreety Jha <138106684+supreetyjha@users.noreply.github.com>
2026-08-07 22:22:23 +0530

    Time: 1 ms (99.76%), Space: 47.2 MB (76.78%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0,curr=0,farthest=0;
+        for(int i=0;i<nums.length-1;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            if(i==curr){
+                jumps++;
+                curr=farthest;
+            }
+        }
+        return jumps;
+    }
+}
