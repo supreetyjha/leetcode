423a7c65bfe89f135600f65c75004f059c87ca56
Supreety Jha <138106684+supreetyjha@users.noreply.github.com>
2026-08-26 21:54:44 +0530

    Time: 5 ms (81.97%), Space: 45.6 MB (94.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,10 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
       int d,rev=0;
       int n=x;
-      if(n<0)
-      {
-        return false;
-      }
+      if(n<0) return false;
       else
       {
         while(n!=0)
@@ -14,7 +11,8 @@ class Solution {
         rev=rev*10 +d;
         n=n/10;
       }
-      return x==rev;   }    
+      return x==rev || x==rev/10;  } 
+         
         
     }
 }
