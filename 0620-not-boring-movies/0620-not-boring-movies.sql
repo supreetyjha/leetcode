0ead3ec6d89a9bbb11e7faa7c94b6c7b028afa49
Supreety Jha <138106684+supreetyjha@users.noreply.github.com>
2026-09-10 19:58:35 +0530

    Time: 266 ms (80.05%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-Select c.id,c.movie,c.description,c.rating
-from Cinema c
-Where c.id % 2 !=0 And c.description != "boring"
-Order by c.rating desc;
+select id, movie, description, rating
+from Cinema
+where (id % 2 = 1) and (description != 'boring')
+order by rating DESC;
